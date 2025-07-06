6aab5f03f119159b478ca0a7a281cd9766fdb4ac
Dineshwar <133356394+dinonomous@users.noreply.github.com>
2025-07-06 18:20:18 +0530

    new fe and proper types for API

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
       >
         <AuthProvider>
           <ModelProvider>
-            <HeaderWithAuth />
             {children}
           </ModelProvider>
         </AuthProvider>
